763d5a6f628bbf6c53864cc2bb33b028e776f329
aqwertyuiop48 <aqwertyuiop48@outlook.com>
2025-11-16 11:50:19 +0530

    C

--- src/main.ts
+++ src/main.ts
@@ -255,11 +255,11 @@ return this;
     </div>
     <script>
     var images = [
-        { src: 'https://raw.githubusercontent.com/sreegithub19/JavaScript-Applications/main/public/puzzle/img/neeruti.jpg', title: 'Puzzle_1' },
-        { src: 'https://raw.githubusercontent.com/sreegithub19/JavaScript-Applications/main/public/puzzle/img/harju_madise.jpg', title: 'Puzzle_2' },
-        { src: 'https://raw.githubusercontent.com/sreegithub19/JavaScript-Applications/main/public/puzzle/img/rahumae.jpg', title: 'Puzzle_3' },
-        { src: 'https://raw.githubusercontent.com/sreegithub19/JavaScript-Applications/main/public/puzzle/img/kakumae.jpg', title: 'Puzzle_4' },
-        { src: 'https://raw.githubusercontent.com/sreegithub19/JavaScript-Applications/main/public/puzzle/img/kohila.jpg', title: 'Puzzle_5' }
+        { src: 'https://raw.githubusercontent.com/aqwertyuiop48/JavaScript-Applications/main/public/puzzle/img/neeruti.jpg', title: 'Puzzle_1' },
+        { src: 'https://raw.githubusercontent.com/aqwertyuiop48/JavaScript-Applications/main/public/puzzle/img/harju_madise.jpg', title: 'Puzzle_2' },
+        { src: 'https://raw.githubusercontent.com/aqwertyuiop48/JavaScript-Applications/main/public/puzzle/img/rahumae.jpg', title: 'Puzzle_3' },
+        { src: 'https://raw.githubusercontent.com/aqwertyuiop48/JavaScript-Applications/main/public/puzzle/img/kakumae.jpg', title: 'Puzzle_4' },
+        { src: 'https://raw.githubusercontent.com/aqwertyuiop48/JavaScript-Applications/main/public/puzzle/img/kohila.jpg', title: 'Puzzle_5' }
     ];
     
     $(function () {
@@ -8316,7 +8316,7 @@ document.write(`
         body {
             margin: 0px;
             padding: 0px;
-            background: #E10A04 url('https://raw.githubusercontent.com/sreegithub19/JavaScript-Applications/main/public/sudoku/images/backgroundx.png') repeat-x;
+            background: #E10A04 url('https://raw.githubusercontent.com/aqwertyuiop48/JavaScript-Applications/main/public/sudoku/images/backgroundx.png') repeat-x;
             font-family: Arial;
             color: #FFAEAE;
             font-size: 12px;
@@ -8338,7 +8338,7 @@ document.write(`
         }
 
         #Wrapper {
-            background: url('https://raw.githubusercontent.com/sreegithub19/JavaScript-Applications/main/public/sudoku/images/background.png') no-repeat top center;
+            background: url('https://raw.githubusercontent.com/aqwertyuiop48/JavaScript-Applications/main/public/sudoku/images/background.png') no-repeat top center;
             min-height: 720px;
         }
 
@@ -8504,7 +8504,7 @@ document.write(`
             border: 0px;
             min-width: 162px;
             min-height: 34px;
-            background: url('https://raw.githubusercontent.com/sreegithub19/JavaScript-Applications/main/public/sudoku/images/submitBackground.png') repeat-x;
+            background: url('https://raw.githubusercontent.com/aqwertyuiop48/JavaScript-Applications/main/public/sudoku/images/submitBackground.png') repeat-x;
             margin: 8px 0px;
             cursor: pointer;
         }
@@ -8517,7 +8517,7 @@ document.write(`
             border: 0px;
             min-width: 28px;
             min-height: 28px;
-            background: url('https://raw.githubusercontent.com/sreegithub19/JavaScript-Applications/main/public/sudoku/images/submitSmallBackground.png') repeat-x;
+            background: url('https://raw.githubusercontent.com/aqwertyuiop48/JavaScript-Applications/main/public/sudoku/images/submitSmallBackground.png') repeat-x;
             margin: 8px 0px;
             padding: 2px 16px;
             cursor: pointer;
@@ -8540,7 +8540,7 @@ document.write(`
             position: absolute;
             top: 0px;
             right: 0px;
-            background: url('https://raw.githubusercontent.com/sreegithub19/JavaScript-Applications/main/public/sudoku/images/shade.png') repeat;
+            background: url('https://raw.githubusercontent.com/aqwertyuiop48/JavaScript-Applications/main/public/sudoku/images/shade.png') repeat;
         }
 
         #youWon div {
@@ -8601,12 +8601,12 @@ document.write(`
                     <br />
                     <br />
                     <div id="calculating" style="display: none;">
-                        <img src="https://raw.githubusercontent.com/sreegithub19/JavaScript-Applications/main/public/sudoku/images/iconTime.png" alt="Calculating Clock" /><span class="finishedLabel">Solving
+                        <img src="https://raw.githubusercontent.com/aqwertyuiop48/JavaScript-Applications/main/public/sudoku/images/iconTime.png" alt="Calculating Clock" /><span class="finishedLabel">Solving
                             puzzles...</span>
                     </div>
                     <div role="tab" aria-selected="true" aria-controls="perfResults" tabindex="200"
                         id="finishedCalculating" style="display: none;">
-                        <img src="https://raw.githubusercontent.com/sreegithub19/JavaScript-Applications/main/public/sudoku/images/iconTime.png" alt="Calculating Clock" /><span class="finishedLabel">Finished
+                        <img src="https://raw.githubusercontent.com/aqwertyuiop48/JavaScript-Applications/main/public/sudoku/images/iconTime.png" alt="Calculating Clock" /><span class="finishedLabel">Finished
                             In:</span>
                         <br />
                         <span id="timeFinished"></span>
